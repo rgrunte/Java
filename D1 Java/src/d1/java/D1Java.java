2b73f277aabc9e138c933197de9a90e510baa975
Reinis Grunte <reinis.grunte@gmail.com>
2019-02-18 21:12:50 +0200

    Update D1Java.java

--- D1 Java/src/d1/java/D1Java.java
+++ D1 Java/src/d1/java/D1Java.java
@@ -31,7 +31,7 @@ public class D1Java {
 //        System.out.println("Ievadiet savu vārdu: ");
 //        String vards = sc.nextLine();
 //        System.out.println("Jūsu vārds ir: " + vards);
-        vardaIevade();
+          loginsUnParole();
     }
     
     private static void vardaIevade(){
@@ -42,5 +42,96 @@ public class D1Java {
         String ievade = sc.nextLine();
         System.out.println("Sveiks, " + ievade);
         
+    }
+    private static void zarosanas(){
+        
+          int a = 5;
+//        if(a==5){
+//            
+//        }else{
+//            
+//        }
+//        
+//        String b = "c";
+//        String d = "aa";
+//        
+//        if(b.equals("a")){
+//            
+//        }else if (b.equals(d)){
+//            
+//        }else{
+//            
+//        }
+        switch(a){
+            case 1:
+                //kas notiek
+                break;
+            case 2:
+                break;
+            default:
+                break;
+                
+            }
+    }
+    
+    private static void uzdevumsZarosanas(){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Ievadiet skaitli: ");
+        int sk = sc.nextInt();
+        if(sk >= 5){
+            System.out.println("**");
+        } else {
+            System.out.println("*");
+        }
+        
+//        String simboli = "aaaa";
+//        int a = simboli.length();
+    }
+    
+    private static void loginsUnParole(){
+//        String logins = "lietotajs";
+//        String parole = "abc123";
+        
+        //datoram jāprasa lai cilvēks ievada loginu un paroli
+        //ja ir kāda kļūda, tad programma beidzās
+        //ja viss ir kārtībā tad ir jāparbauda vai sakrīt ar to kas ir ievadits
+        //vai tas kas ir ievadits 3-12 simboliem
+        //paroles vietā 5-10 simboli
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Ievadiet loginu: ");
+        String logins = sc.nextLine();
+        int a = logins.length();
+        
+        if(a < 3 || a > 12){
+            System.out.println("logins ir nepareizs");
+        } else {
+            System.out.println("logins ir pareizs");
+        }
+        
+        System.out.print("Ievadiet paroli: ");
+        String parole = sc.nextLine();
+        int b = parole.length();
+        if(b < 5 || b > 10){
+            System.out.println("parole ir nepareiza");
+        } else {
+            System.out.println("parole ir pareiza");
+        }
+        
+//        System.out.print("Ievadiet loginu: ");
+//        String logins = sc.nextLine();
+//        int a = logins.length();
+//        
+//        System.out.print("Ievadiet paroli: ");
+//        String parole = sc.nextLine();
+//        int b = parole.length();
+//
+//        if(a < 3 || a > 12 && b < 5 || b > 10){
+//            System.out.println("logins un/vai parole ir nepareiza");
+//        } else {
+//            System.out.println("logins un parole ir pareiza");
     }
 }
